a1c2665093d2895a2aed9248f539711d0195cd91
Xiong Zhang <xzhang@tsinghua.edu.cn>
2017-08-15 16:14:44 +0800

    Optimize the solver.

--- src/cpp/Solver.cpp
+++ src/cpp/Solver.cpp
@@ -55,50 +55,39 @@ void LDLTSolver::LDLT()
 
 	for (int j = 2; j <= N; j++)      // Loop for column 2:n (Numbering starting from 1)
 	{
-		// Array containing elements in column j
-		double* Columnj = &K[Address[j-1] - 1];
-
-		// Address of K_jj in banded matrix (Numbering starting from 0);
-		int Address_jj = Address[j-1] - 1;
-
         // Row number of the first non-zero element in column j (Numbering starting from 1)
 		int mj = j - (Address[j] - Address[j-1]) + 1;
+
+		// Address of K_jj and K_mj,j in banded matrix (Numbering starting from 0);
+		int Address_jj = Address[j-1] - 1;
+		int Address_mjj = Address[j-1] + j - mj - 1;
         
 		for (int i = mj+1; i <= j-1; i++)	// Loop for mj+1:j-1 (Numbering starting from 1)
 		{
             // Row number of the first nonzero element in column i (Numbering starting from 1)
 			int mi = i - (Address[i] - Address[i-1]) + 1;
             
-			int mij = max(mi, mj);
+			int mm = max(mi, mj);
 
-			// Array containing elements in column i
-			double* Columni = &K[Address[i-1] - 1];
+			// Address of L_mm,i and U_mm,j in banded matrix (Numbering starting from 0)
+			int Address_mmi = Address[i-1] + i - mm - 1;
+			int Address_mmj = Address[j-1] + j - mm - 1;
             
 			double C = 0.0;
-			for (int r = mij; r <= i-1; r++)	// Loop for max(mi,mj):i-1 (Numbering starting from 1)
-			{
-				// Address of L_ri and U_rj in banded matrix (Numbering starting from 0)
-				int Address_ri = Address[i-1] + i - r - 1;
-				int Address_rj = Address[j-1] + j - r - 1;
+			for (int r = mm; r <= i-1; r++)	// Loop for max(mi,mj):i-1 (Numbering starting from 1)
+				C += K[Address_mmi++] * K[Address_mmj++];		// C += L_ri * U_rj
 
-				C += K[Address_ri] * K[Address_rj];		// C += L_ri * U_rj
-			}
-
-			// Address of K[i,j] in banded matrix (Numbering starting from 0)
-			int Address_ij = Address[j-1] + j - i - 1;
-
-			K[Address_ij] = K[Address_ij] - C;
+			K[++Address_mjj] = K[Address_mjj] - C;	// U_ij = K_ij - C
 		}
+
+		// Address of K_mj,j in banded matrix (Numbering starting from 0);
+		Address_mjj = Address[j-1] + j - mj - 1;
 
 		for (int r = mj; r <= j-1; r++)	// Loop for mj:j-1 (column j)
 		{
-			// Address of U_rj and D_rr in banded matrix (Numbering starting from 0)
-			int Address_rj = Address[j-1] + j - r - 1;	
-			int Address_rr = Address[r-1] - 1;
-
-			double Lrj = K[Address_rj] / K[Address_rr];	// L_rj = U_rj / D_rr
-			K[Address_jj] = K[Address_jj]  - Lrj * K[Address_rj];	// D_jj = K_jj - sum(L_rj*U_rj, r=mj:j-1)
-			K[Address_rj] = Lrj;
+			double Lrj = K[Address_mjj] / K[Address[r-1] - 1];	// L_rj = U_rj / D_rr
+			K[Address_jj] = K[Address_jj]  - Lrj * K[Address_mjj];	// D_jj = K_jj - sum(L_rj*U_rj, r=mj:j-1)
+			K[Address_mjj++] = Lrj;
 
 			if (fabs(K[Address_jj]) <= FLT_MIN)
 			{
@@ -121,30 +110,24 @@ void LDLTSolver::BackSubstitution()
 	unsigned int* Address = FEMData->GetDiagonalAddress(); // Numering starting from 1
 	unsigned int N = FEMData->GetNEQ();
 
-//	Reduce right-hand-side load vector
+//	Reduce right-hand-side load vector (LV = R)
 	for (int i = 2; i <= N; i++)	// Loop for i=2:N (Numering starting from 1)
 	{
 		int mi = i - (Address[i] - Address[i-1]) + 1;
 
 		for (int j = mi; j <= i-1; j++)	// Loop for j=mi:i-1
-		{
-			int Address_ji = Address[i-1] + i - j - 1;
-			Force[i-1] = Force[i-1] - K[Address_ji] * Force[j-1];
-		}
+			Force[i-1] = Force[i-1] - K[Address[i-1] + i - j - 1] * Force[j-1];	// V_i = R_i - sum_j (L_ji V_j)
 	}
 
-//	Back substitute
+//	Back substitute (Vbar = D^(-1) V, L^T a = Vbar)
 	for (int i = 1; i <= N; i++)	// Loop for i=1:N
-		Force[i-1] = Force[i-1] / K[Address[i-1] - 1];
+		Force[i-1] = Force[i-1] / K[Address[i-1] - 1];	// Vbar = D^(-1) V
 
 	for (int j = N; j >= 2; j--)	// Loop for j=N:2
 	{
 		int mj = j - (Address[j] - Address[j-1]) + 1;
 
 		for (int i = mj; i <= j-1; i++)	// Loop for i=mj:j-1
-		{
-			int Address_ij = Address[j-1] + j - i - 1;
-			Force[i-1] -= K[Address_ij] * Force[j-1];
-		}
+			Force[i-1] -= K[Address[j-1] + j - i - 1] * Force[j-1];	// a_i = Vbar_i - sum_j(L_ij Vbar_j)
 	}
 };
